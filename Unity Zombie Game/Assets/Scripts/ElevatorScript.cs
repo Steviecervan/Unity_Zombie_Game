bba89f6a7c154a49d0186a9812652e246b83b899
Stevie-Cervan <stevie.cervan06@gmail.com>
2021-12-28 10:58:53 -0700

    Uploaded Elevator Script

---
+++ Unity Zombie Game/Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+
+    public GameObject elevatorCallButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
